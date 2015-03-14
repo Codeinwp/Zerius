c83d47f25a983bca3f7a7e5eb807d84cae544d2e
rodica-andronache <rodica.andronache@vertistudio.com>
2015-03-14 12:49:38 -0700

    !!! Fixed prefix for functions for wp.org review

--- functions.php
+++ functions.php
@@ -2,21 +2,21 @@
 
 // add any new or customised functions here
 
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-function theme_enqueue_styles() {
-
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array('zerif_bootstrap_style') );
+add_action( 'wp_enqueue_scripts', 'zerius_enqueue_styles' );
+function zerius_enqueue_styles() {
+	
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array('zerif_bootstrap_style') );
 
 	// Loads our main stylesheet.
-	wp_enqueue_style( 'zerif-child-style', get_stylesheet_uri(), array('zerif_style'), 'v1' );
+	 wp_enqueue_style( 'zerif-child-style', get_stylesheet_uri(), array('zerif_style'), 'v1' );
 
 }
 
-function remove_style_child(){
+function zerius_remove_style_child(){
 	remove_action('wp_print_scripts','zerif_php_style');	
 }
 
-add_action( 'wp_enqueue_scripts', 'remove_style_child', 100 );
+add_action( 'wp_enqueue_scripts', 'zerius_remove_style_child', 100 );
 
 
 function zerius_custom_script_fix()
